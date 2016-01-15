7e8fb495536d001cfc7dee3bb2c17af871de1975
sato <post.sato+train@gmail.com>
2016-01-15 04:04:39 +0000

    facebook login function

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do 
-  provider :facebook,"App ID","App Secret",
-           :scope => 'email,read_stream', display => 'popup'
+  # provider :facebook,"App ID","App Secret",
+  #         :scope => 'email,read_stream', display => 'popup'
 end 
